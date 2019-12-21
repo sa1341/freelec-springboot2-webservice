4b0d791f467713010c336d3b505ee352791e60bb
임준영 <a79007714@gmail.com>
2019-12-22 00:08:48 +0900

    실행권한 다시 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,15 +29,11 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod+x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-
-    -Dspring.profiles.active=real \
-
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-

91c957ac147fa991aab8e4c99e33925c540f9edf
임준영 <a79007714@gmail.com>
2019-12-21 23:50:21 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,6 @@ else
     sleep 5
 fi
 
-
 echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -35,6 +34,10 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
+
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+
     -Dspring.profiles.active=real \
+
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
